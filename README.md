17221e9def0eaafbbb5f520d91b3d61072f993eb
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2024-04-23 21:10:15 +0300

    create composable use Fetch

--- README.md
+++ README.md
@@ -1,29 +1,4 @@
+page 210 
 
-## Project Setup
 
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
-
-shad cn
-https://www.youtube.com/watch?v=vs-vfc9nt0k
-
-7.48
+https://github.com/mayashavin/learning-vue-app/blob/main/ch05/src/composables/useFetch.ts
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,7 @@
 <script setup lang="ts">
- import Button from '@/components/ui/button/Button.vue'
- import Base from '@/components/common/Base.vue'
- import BaseWithInterface from '@/components/common/BaseWithInterface.vue'
- import BaseExportPropsFromFile from '@/components/common/BaseExportPropsFromFile.vue'
- import { ref } from 'vue'
- import BaseTypingReactive from '@/components/common/BaseTypingReactive.vue'
- import BaseTypingComputed from '@/components/common/BaseTypingComputed.vue'
- import InputEventHandler from '@/components/common/InputEventHandler.vue'
- import TypingLinkRef from '@/components/common/TypingLinkRef.vue'
 
-
- // typing ref
- const year = ref<string | number>('2022');
-
- console.log(year.value)
-
- const n = ref<number>(1);
-
- console.log(n.value)
 </script>
 
 <template>
-  <div class="container p-8">
-    <Button>Button</Button>
-    <br>
-    <Base foo="somefoo" bar="bar" />
-    <br>
-    <BaseWithInterface foo="111" :bar="22+5" />
-    <BaseExportPropsFromFile   _id="1" local-path="url" url="rrrr"/>
-    <BaseTypingReactive/>
-    <br>
-    <BaseTypingComputed/>
-    <br>
-    <InputEventHandler/>
-    <br>
-    <TypingLinkRef />
-  </div>
+rtr
 </template>
